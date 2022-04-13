faddae0523f93e1215282938c984e5ac1087036f
Haesun Lee <hlee830@gatech.edu>
2022-04-13 17:55:37 -0400

    changed client_id

--- starter_files-1/client.py
+++ starter_files-1/client.py
@@ -4,7 +4,7 @@ import json
 import argparse
 
 if __name__ == "__main__":
-    client_id = None    # need to change to what..?
+    client_id = '1'    # need to change to what..?
     content = None
     actions = None
 
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     
     #### Code added by HS ####
     Filename = client_id
-    Format = ".json"
+    Format = '.json'
     Filename = Filename + Format
     
     with open(Filename) as f:
